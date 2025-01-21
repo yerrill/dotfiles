0403ad18ddc448cea6f3c3a2d5e49377d581fd72
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2025-01-21 18:54:28 -0500

    common 2

--- common.nix
+++ common.nix
@@ -8,10 +8,10 @@
     packages = with pkgs; [
 
     ]
-  }
+  };
 
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [
     curl
