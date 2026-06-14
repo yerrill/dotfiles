65be662e842d9de7dd28145f830e57c47bcc14f2
Zach Yerrill <zyerrill@pm.me>
2026-06-13 22:27:53 -0400

    nvim updates and mdfmt

--- nvim/lua/config/lang.lua
+++ nvim/lua/config/lang.lua
@@ -1,19 +1,31 @@
+-- Format Router
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("BlackFormat", { clear = true }),
+    -- pattern = "*.py",
+    callback = function(args)
+        supported = {
+            ["python"] = python_format,
+        }
+
+        if supported[vim.bo.filetype] then
+            supported[vim.bo.filetype](args)
+        end
+    end,
+})
+
 -- Python
 vim.lsp.enable "pyright"
 vim.keymap.set("n", "<leader>pr", "<cmd>! python %<CR>", { desc = "Run Python file" })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("BlackFormat", { clear = true }),
-  pattern = "*.py",
-  callback = function(args)
+function python_format(args)
     -- Run black on the saved file
     vim.fn.system({ "black", "--quiet", args.file })
     -- Reload the buffer to pick up black's changes
     vim.api.nvim_buf_call(args.buf, function()
-      vim.cmd("silent! edit")
+        vim.cmd("silent! edit")
     end)
-  end,
-})
+end
+
 
 -- Lua
 vim.lsp.enable "lua_ls"
